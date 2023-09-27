87e09f9701b129da7e55c4d4601aee117927a74c
chonsawat <chonsawat.nakanam@kkumail.com>
2023-09-27 22:58:42 +0700

    COMPONENT SCANNING

--- 02-spring-boot-3-spring-core/02-component-scanning/src/main/java/com/example/demo/SpringCoreApplication.java
+++ 02-spring-boot-3-spring-core/02-component-scanning/src/main/java/com/example/demo/SpringCoreApplication.java
@@ -3,7 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.example.demo",
+                "com.example.utils"
+        }
+)
 public class SpringCoreApplication {
 
     public static void main(String[] args) {
--- 02-spring-boot-3-spring-core/02-component-scanning/src/main/java/com/example/demo/common/CricketCoach.java
+++ 02-spring-boot-3-spring-core/02-component-scanning/src/main/java/com/example/demo/common/CricketCoach.java
@@ -1,9 +1,10 @@
-package com.example.demo;
+package com.example.demo.common;
 
+import com.example.utils.Coach;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Fast bowling for 15 minutes";
