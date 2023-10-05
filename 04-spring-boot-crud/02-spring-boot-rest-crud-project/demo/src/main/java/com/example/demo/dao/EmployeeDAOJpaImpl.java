5aa89d0a42945c96df888dfdbd66c50318e17403
chonsawat <chonsawat.nakanam@kkumail.com>
2023-10-05 23:52:00 +0700

    DAO

---
+++ 04-spring-boot-crud/02-spring-boot-rest-crud-project/demo/src/main/java/com/example/demo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,28 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.Employee;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO{
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("FROM Employee", Employee.class);
+        return query.getResultList();
+    }
+}
---
+++ 04-spring-boot-crud/02-spring-boot-rest-crud-project/demo/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
